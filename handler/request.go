b36f92c1c7a5a89a2a725fa3d457f77b7aca09e6
Thuany Mendonca <thuany1997@hotmail.com>
2023-11-01 10:19:53 -0300

    feat(struct) add reques and response

---
+++ handler/request.go
@@ -0,0 +1,50 @@
+package handler
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func errParamIsRequired(name, typ string) error {
+	return fmt.Errorf("param: %s (type: %s) is required", name, typ)
+}
+
+// CreateOpening
+type CreateOpeningRequest struct {
+	Role     string `json:"role"`
+	Company  string `json:"company"`
+	Location string `json:"location"`
+	Remote   *bool  `json:"remote"`
+	Link     string `json:"link"`
+	Salary   int64  `json:"salary"`
+}
+
+func (r *CreateOpeningRequest) Validate() error {
+	if r.Role == "" {
+		// exemplo utilizando recurso da linguagem para obter o tipo
+		valueType := reflect.TypeOf(r.Role)
+		return errParamIsRequired("role", valueType.String())
+	}
+
+	if r.Company == "" {
+		return errParamIsRequired("company", "string")
+	}
+
+	if r.Location == "" {
+		return errParamIsRequired("location", "string")
+	}
+
+	if r.Link == "" {
+		return errParamIsRequired("link", "string")
+	}
+
+	if r.Remote == nil {
+		return errParamIsRequired("remote", "bool")
+	}
+
+	if r.Salary <= 0 {
+		return errParamIsRequired("salary", "int")
+	}
+
+	return nil
+}
