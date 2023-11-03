a8913fd5945853e75e658c8f9a09291ff75f0336
Thuany Mendonca <thuany1997@hotmail.com>
2023-11-02 23:17:03 -0300

    feat(api) add delete, list and show opening

--- handler/deleteOpening.go
+++ handler/deleteOpening.go
@@ -1,13 +1,40 @@
 package handler
 
 import (
+	"errors"
+	"fmt"
 	"net/http"
 
+	"github.com/ThuanyMendonca/gooportunities/schemas"
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
 func DeleteOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "Hello DELETE",
-	})
+	id := ctx.Query("id")
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, errParamIsRequired("id", "queryParameter").Error())
+		return
+	}
+
+	opening := schemas.Opening{}
+
+	// Find Opening
+	if err := db.First(&opening, id).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			sendError(ctx, http.StatusNotFound, fmt.Sprintf("opening with id %s not found", id))
+			return
+		}
+
+		sendError(ctx, http.StatusInternalServerError, "an internal error occurred")
+		return
+	}
+
+	// Delete Opening
+	if err := db.Delete(&opening).Error; err != nil {
+		sendError(ctx, http.StatusInternalServerError, fmt.Sprintf("error deleting opening with id: %s", id))
+		return
+	}
+
+	sendSuccess(ctx, "delete-opening", opening)
 }
--- handler/showOpening.go
+++ handler/showOpening.go
@@ -1,13 +1,30 @@
 package handler
 
 import (
+	"errors"
 	"net/http"
 
+	"github.com/ThuanyMendonca/gooportunities/schemas"
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
 func ShowOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "Hello GET",
-	})
+	id := ctx.Query("id")
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, errParamIsRequired("id", "queryParameter").Error())
+		return
+	}
+
+	opening := schemas.Opening{}
+	if err := db.First(&opening, id).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			sendError(ctx, http.StatusNotFound, "opening not found")
+			return
+		}
+		sendError(ctx, http.StatusInternalServerError, "internal server error")
+		return
+	}
+
+	sendSuccess(ctx, "show-opening", opening)
 }
