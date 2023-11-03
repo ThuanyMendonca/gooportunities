b3afba262d22b0d9c7795ff5c8b9a35c06d7d070
Thuany Mendonca <thuany1997@hotmail.com>
2023-11-02 23:16:40 -0300

    feat(update) add update opening

--- handler/updateOpening.go
+++ handler/updateOpening.go
@@ -1,13 +1,75 @@
 package handler
 
 import (
+	"errors"
+	"fmt"
 	"net/http"
 
+	"github.com/ThuanyMendonca/gooportunities/schemas"
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
 func UpdateOpeningsHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "Hello UPDATE",
-	})
+	id := ctx.Query("id")
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, errParamIsRequired("id", "queryParameter").Error())
+		return
+	}
+
+	request := UpdateOpeningRequest{}
+
+	ctx.BindJSON(&request)
+
+	if err := request.Validate(); err != nil {
+		logger.Errorf("validation error: %v", err.Error())
+		sendError(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	opening := schemas.Opening{}
+
+	if err := db.First(&opening, id).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			sendError(ctx, http.StatusNotFound, fmt.Sprintf("opening with id %s not found", id))
+			return
+		}
+
+		sendError(ctx, http.StatusInternalServerError, "an internal error occurred")
+		return
+	}
+
+	// Update opening
+	if request.Role != "" {
+		opening.Role = request.Role
+	}
+
+	if request.Company != "" {
+		opening.Company = request.Company
+	}
+
+	if request.Link != "" {
+		opening.Link = request.Link
+	}
+
+	if request.Location != "" {
+		opening.Location = request.Location
+	}
+
+	if request.Remote != nil {
+		opening.Remote = *request.Remote
+	}
+
+	if request.Salary > 0 {
+		opening.Salary = request.Salary
+	}
+
+	// Save opening
+	if err := db.Save(&opening).Error; err != nil {
+		logger.Errorf("error updating opening: %v", err.Error())
+		sendError(ctx, http.StatusInternalServerError, "error updating opening")
+		return
+	}
+
+	sendSuccess(ctx, "update-opening", opening)
 }
